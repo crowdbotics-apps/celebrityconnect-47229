f5754b7d7f5b38a59dfd987d583686c0569133a5
Crowdbotics <team@crowdbotics.com>
2024-03-22 11:50:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "celebrityconnect-47229",
+  url: "https://celebrityconnect-47229.botics.co",
+  api: "https://celebrityconnect-47229.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
